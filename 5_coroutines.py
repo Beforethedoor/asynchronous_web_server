f07f734b223806efdc045206b7d1d3bf54719128
Sergei Iniakin <ultraphen@192.168.1.3>
2023-05-19 21:50:16 +0300

    add coroutines

---
+++ 5_coroutines.py
@@ -0,0 +1,50 @@
+
+
+class BlaBlasExceprion(Exception):
+    pass
+
+
+def coroutine(func):
+    def inner(*args, **kwargs):
+        g = func(*args, **kwargs)
+        g.send(None)
+        return g
+    return inner
+
+
+def subgen():
+    x = "Ready to accept message"
+    message = yield x
+    print(message)
+
+
+@coroutine
+def average():
+    count = 0
+    summ = 0
+    average = None
+
+    while True:
+        try:
+            x = yield average
+        except StopIteration:
+            print("Done")
+            break
+        except BlaBlasExceprion:
+            print('BlaBlasExceprion')
+            break
+        else:
+            count += 1
+            summ += x
+            average = round(summ / count, 2)
+
+    return average
+
+
+g = average()
+print(g.send(10))
+
+try:
+    g.throw(StopIteration)
+except StopIteration as e:
+    print(e.value)
